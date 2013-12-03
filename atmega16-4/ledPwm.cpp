0d495add79f634164e40ec71f0e634b4a30c5bff
Robert <201292@live.nl>
2013-12-03 20:52:34 +0100

    doing more pwm, not making any progress heregit add .

--- atmega16-4/ledPwm.cpp
+++ atmega16-4/ledPwm.cpp
@@ -4,24 +4,35 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#include <stdio.h>
+
 #include "uart.cpp"
 
-char formated[21] = 0;
-
+char formated[21];
 int main(void)
 {
-	DDRB |= (1<<PB0);
+	DDRB |= 0xFF;
 	//default baud I use is 9600
 	init_uart();
+	
+	TCCR1B |= (1<<CS10);
+	
+	int pwmMax = 255;
+	int pwmOn = 10;
+	
 	while(1)
 	{
-		
+		if(TCNT1 > 0)
+		{
+			PORTB |= 0xFF;
+		}
+		else
+		{
+			PORTB |= 0xFF;
+		}
+		sprintf(formated, "OCR1A: %d TCNT1: %d \n", OCR1A, TCNT1);
+		uart_put_str(formated);
 	}
-}
-
-ISR(TIMER1_OVF_vect)
-{
-	PORTB ^= (1<<PB0);
 }
 
 ISR(USART_RXC_vect)
