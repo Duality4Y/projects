df80b9252215cc6c84e740abdb752826e6726d76
Robert <201292@live.nl>
2013-12-03 16:23:42 +0100

    this

--- atmega16-4/ledPwm.cpp
+++ atmega16-4/ledPwm.cpp
@@ -1,21 +1,18 @@
-#define F_CPU 8000000
+#define F_CPU 1000000
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 int main(void)
 {
-	//led on PD7, set as output.
-	DDRD |= (1<<PD7);
-	//set prescaler 1
-	TCCR1B |= (1<<CS10)|(1<<CS11);
-	while(1)
-	{
-		if(TCNT1 >= 15624)
-		{
-			PORTB ^= (1<<0);
-			
-			TCNT1 = 0;
-		}
-	}
-	return 1;
+	DDRB |= (1<<PB0);
+	TIMSK |= (1<<TOIE1);
+	TCCR1B |= (1<<CS11);
+	sei();
+	while(1){}
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	PORTB ^= (1<<PB0);
 }
