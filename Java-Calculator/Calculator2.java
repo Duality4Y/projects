dbb343b1fa48ed3eb07f08277b30318ed2670b8d
Robert <201292@live.nl>
2014-02-13 01:33:19 +0100

    just a save

--- Java-Calculator/Calculator2.java
+++ Java-Calculator/Calculator2.java
@@ -1,51 +1,123 @@
+package layout;
+
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 class Calculator2 extends JFrame
 {
 	private JPanel panel;
-	private JLabel label;
-	private JButton button;
-	
-	private numberOfButtons = 0;
 	
 	char[] buttonContent = 
 	{
-		
+		'1','2','3','/',
+		'4','5','6','*',
+		'7','8','9','-',
+		'C','0','.','+',
+		' ',' ',' ','=',
 	};
+	private JTextArea output = new JTextArea(1,15);
+	ArrayList<JButton> buttons = new ArrayList();
+	
+	private String inputString = "";
+	private int result = 0;
+	private String operator = "";
 	
 	public Calculator2()
 	{
 		panel = new JPanel();
-		label = new JLabel("Hartelijk Welkom!");
-		button = new JButton("klik");
 		
+		//create hanlers.
 		buttonhandler handler = new buttonhandler();
-		button.addActionListener(handler);
+		TextFieldListener tfListener = new TextFieldListener();
 		
-		panel.add(label);
-		panel.add(button);
-		
+		//add textoutput to panel.
+		panel.add(output);
+		//create buttons.
+		for(int i = 0;i<buttonContent.length;i++)
+		{
+			buttons.add(i, new JButton(buttonContent[i]+""));
+		}
+		//for every button add it to panel,
+		//add it to actionlistener and bind it to handler.
+		//if button has text " " don't show it but still place it.
+		for(JButton button: buttons)
+		{
+			//add button to panel.
+			panel.add(button);
+			//apply size to all buttons.
+			button.setSize(5,5);
+			//if the button has no text, make it invisible.
+			//and don't add it to action listener.
+			if(button.getText().equals(" "))
+			{
+				button.setBorderPainted(false);
+				button.setOpaque(false);
+				button.setContentAreaFilled(false);
+				button.setEnabled(false);
+			}
+			else
+			{
+				button.addActionListener(handler);
+			}
+		}
+		//shows content on panel?
 		setContentPane(panel);
 		
-		
-		//this.main();
 	}
 	
 	public static void main(String args[])
 	{
 		JFrame frame = new Calculator2();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(400,200);
-		frame.setTitle("this Title" );
+		frame.setTitle("Calc" );
+		frame.setSize(210,230);
+		//frame.setResizable(false);
 		frame.setVisible(true);
+	}
+	
+	class TextFieldListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+		}
 	}
 	
 	class buttonhandler implements ActionListener
 	{
-		public void actionPerformed(ActionEvent e)
+		public void actionPerformed(ActionEvent event)
 		{
-			label.setText("Bedankt!");
+			String action = event.getActionCommand();
+		}
+		public void printPressed(String action)
+		{
+			System.out.println(action+" pressed");
+			output.setText(output.getText()+" "+action);
 		}
 	}
+	/*
+	class Calculate
+	{
+		public double calculate(String operator, double value)
+		{
+			case 
+		}
+		public double add(double value1, double value2)
+		{
+			
+		}
+		public double subtract(double value1, double value2)
+		{
+			
+		}
+		public double divide(double value1, double value2)
+		{
+			
+		}
+		public double multiply(double value1, double value2)
+		{
+			
+		}
+	}*/
 }
