fc5cea1644fbf9f8cb1cee772f32913afba443ca
Robert <201292@live.nl>
2014-03-20 13:28:29 +0100

    worked on javatest.java

--- Java-Calculator/javatest.java
+++ Java-Calculator/javatest.java
@@ -1,27 +1,29 @@
+class Moeder
+{
+	public Moeder(){}
+}
+interface Vader
+{
+	//public Vader(){}
+}
+	
+class Kind extends Moeder implements Vader
+{
+	public Kind(){}
+	//public void vader(){int i = 0;}
+}
+	
 class javatest
 {
-	public class Moeder
-	{
-		public Moeder(){}
-	}
-	
-	public interface Vader
-	{
-		//public Vader(){}
-	}
-	
-	public class Kind extends Moeder implements Vader
-	{
-		public Kind(){}
-	}
-	
 	public javatest()
 	{
 		
 	}
-	public static void main()
+	public static void main(String args[])
 	{
-		//Moeder m = new Moeder();
-		Moeder m = new Vader();
+		Moeder m = new Moeder();
+		Moeder m2 = new Kind();
+		Vader v = new Kind();
+		Kind k = new Vader();
 	}
 }
