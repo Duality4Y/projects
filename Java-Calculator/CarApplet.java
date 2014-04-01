21c7edc5a69390aa2511e82138ec5221a9d9f458
Robert <201292@live.nl>
2014-04-01 15:32:42 +0200

    worked on CarApplet.java

--- Java-Calculator/CarApplet.java
+++ Java-Calculator/CarApplet.java
@@ -6,6 +6,8 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.BorderLayout;
 
+import java.util.*;
+
 class CarApplet extends JFrame
 {
 	JPanel mainPanel;
@@ -25,29 +27,117 @@ class CarApplet extends JFrame
 
 class DrawingPanel
 {
+	
 }
 
 class Car
 {
+	private ArrayList<Part> partsList;
+	
+	public Car(int left, int under, int width, int height)
+	{
+		partsList = new ArrayList<partsList>();
+	}
 }
 
-public interface Part
+interface Part
 {
 	public void draw(Graphics g);
 }
 
-abstract class Part
+abstract class AbstractPart implements Part
 {
+	//make static to associate variables with this class.
+	//and not with the class calling super() and using similair
+	//named variables.
+	protected static int left,under; 
+	protected static Color color;
+	
+	protected AbstractPart()
+	{
+		color = Color.black;
+		left = under = 0;
+	}
+	
+	protected AbstractPart(Color color, int left, int under)
+	{
+		this.color = color;
+		this.left = left;
+		this.under = under;
+	}
+	
+	public abstract void draw(Graphics g);
+	
+	public Color getColor()
+	{
+		return this.color;
+	}
+	
+	public void setColor(Color color)
+	{
+		this.color = color;
+	}
 }
 
-class RectAngle extends Part
+class Circle extends AbstractPart implements Part
 {
+	private int cross_section;
+	
+	public Circle(Color color, int left, int right, int cross_section)
+	{
+		super(color, left, under);
+		this.cross_section = cross_section;
+	}
+	
+	public void draw(Graphics g)
+	{
+		g.setColor(this.color);
+		g.fillOval(left, under-cross_section, cross_section, cross_section);
+		g.setColor(Color.black);
+		g.drawOval(left, under-cross_section, cross_section, cross_section);
+	}
 }
 
-class Circle extends Part
+class RectAngle extends AbstractPart implements Part
 {
+	private int width,height;
+	
+	public RectAngle(Color color, int left, int right, int width, int height)
+	{
+		super(color, left, under);
+		this.width = width;
+		this.height = height;
+	}
+	
+	public void draw(Graphics g)
+	{
+		g.setColor(this.color);
+		g.fillRect(left, under-height, width, height);
+		g.setColor(Color.black);
+		g.drawRect(left, under-height, width, height);
+	}
 }
 
-class TriAngle extends Part
+class TriAngle extends AbstractPart implements Part
 {
+	private int[] xpoints;
+	private int[] ypoints;
+	
+	public TriAngle(Color color, int left, int under, int[] xpoints, int[] ypoints)
+	{
+		super(color,left,under);
+		this.xpoints = xpoints;
+		this.ypoints = ypoints;
+	}
+	
+	public void draw(Graphics g)
+	{
+		if(xpoints.length == ypoints.length)
+		{
+			g.setColor(color);
+			g.fillPolygon(xpoints, ypoints, xpoints.length);
+			g.setColor(Color.black);
+			g.fillPolygon(xpoints, ypoints, xpoints.length);
+		}
+	}
 }
