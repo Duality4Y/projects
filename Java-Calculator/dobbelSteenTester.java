186682a2053aa99998a37d0afbb2a34f7f46216d
Robert <201292@live.nl>
2014-04-10 20:22:30 +0200

    worked on dobbelSteenTester.java

--- Java-Calculator/dobbelSteenTester.java
+++ Java-Calculator/dobbelSteenTester.java
@@ -7,27 +7,23 @@ import java.util.ArrayList;
 class dobbelSteenTester extends JFrame
 {
 	private JPanel mainPanel;
-	private JPanel buttonPanel;
-	private JPanel controle;
+	private JPanel control;
 	private JPanel view;
 	private Model model;
 	
-	private Timer timer;
-	
 	public dobbelSteenTester()
-	{
-		timer = new Timer(20, new TimerHandler());
-		timer.start();
+	{		
+		mainPanel = new JPanel(new BorderLayout());
+		//create a instance of model
+		model = new Model();
+		//create a instance of control and give it a reference to model.
+		control = new Control(model);
+		//create a instance of view.
+		view = new View();
 		
-		mainPanel = new JPanel(new GridLayout(1,2));
-		buttonPanel = new JPanel(new GridLayout(2,1));
-	}
-	class TimerHandler implements ActionListener
-	{
-		public void actionPerformed(ActionEvent e)
-		{
-			model.throwDice();
-		}
+		mainPanel.add(view);
+		mainPanel.add(control, BorderLayout.PAGE_END);
+		setContentPane(mainPanel);
 	}
 	
 	public static void main(String args[])
@@ -46,30 +42,85 @@ class View extends JPanel
 class Model
 {
 	private int thrownNumber = 0;
+	private int throwCount = 0;
 	private Random randomGenerator = new Random();
-	private ArrayList<Integer> numbers; 
+	private ArrayList<Integer> numbers;
+	private Timer timer;
 	public Model()
 	{
 		numbers = new ArrayList<Integer>();
+		
+		timer = new Timer(20, new TimerHandler());
+		timer.start();
+	}
+	class TimerHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			throwDice();
+		}
 	}
 	public void throwDice()
 	{
 		addNumber(randomGenerator.nextInt(8));
+		throwCount++;
+		System.out.println(numbers);
 	}
 	public void addNumber(int num)
 	{
-		//numbers.add(num);
+		numbers.add(num);
 	}
-	public void average()
+	public void startThrowing()
 	{
+		timer.start();
+	}
+	public void stopThrowing()
+	{
+		timer.stop();
+	}
+	public int getAverage(int num)
+	{
+		int total = 0;
 		for(Integer number:numbers)
 		{
-			//System.out.println(number);
+			if(number == num)
+			{
+				total+=number;
+			}
 		}
+		return (int)throwCount/total;
 	}
 }
 
 class Control extends JPanel
 {
-	
+	private JButton startbutton;
+	private JButton stopbutton;
+	private Model model;
+	public Control(Model model)
+	{
+		super();
+		this.model = model;
+		
+		startbutton = new JButton("start");
+		startbutton.addActionListener(new startbuttonHandler());
+		stopbutton = new JButton("stop");
+		stopbutton.addActionListener(new stopbuttonHandler());
+		add(startbutton);
+		add(stopbutton);
+	}
+	class startbuttonHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			model.startThrowing();
+		}
+	}
+	class stopbuttonHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			model.stopThrowing();
+		}
+	}
 }
