4026ebfd3e07713496dce44272161a607bccfb5f
Robert <robert@Robert-Desktop.(none)>
2014-04-10 18:33:14 +0200

    worked on dice mvc

--- Java-Calculator/dobbelSteenTester.java
+++ Java-Calculator/dobbelSteenTester.java
@@ -1,32 +1,72 @@
 import java.awt.event.*;
-import javax.swing.Timer;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
+import java.util.ArrayList;
 
-class dobbelSteenTester
+class dobbelSteenTester extends JFrame
 {
+	private JPanel mainPanel;
+	private JPanel buttonPanel;
+	private JPanel controle;
+	private JPanel view;
+	private Model model;
+	
 	private Timer timer;
+	
 	public dobbelSteenTester()
 	{
-		this.timer = new Timer(20, new TimerHandler());
+		timer = new Timer(20, new TimerHandler());
+		timer.start();
+		
+		mainPanel = new JPanel(new GridLayout(1,2));
+		buttonPanel = new JPanel(new GridLayout(2,1));
 	}
-	class TimerHandler
+	class TimerHandler implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
 		{
 			model.throwDice();
 		}
 	}
+	
+	public static void main(String args[])
+	{
+		dobbelSteenTester dobbelsteen = new dobbelSteenTester();
+		dobbelsteen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		dobbelsteen.setSize(500,500);
+		dobbelsteen.setVisible(true);
+	}
 }
 
-class view
-{}
-class model
+class View extends JPanel
+{
+}
+
+class Model
 {
 	private int thrownNumber = 0;
-	public model()
+	private Random randomGenerator = new Random();
+	private ArrayList<Integer> numbers; 
+	public Model()
 	{
+		numbers = new ArrayList<Integer>();
 	}
-	public void 
+	public void throwDice()
+	{
+		addNumber(randomGenerator.nextInt(8));
+	}
+	public void addNumber(int num)
+	{
+		numbers.add(num);
+	}
+	public void average()
+	{
+		
+	}
 }
-class control{}
+
+class Control extends JPanel
+{
+	
+}
