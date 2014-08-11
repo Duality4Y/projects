de59072fbf90b3c19bb7b6cf30696ec32720736d
Robert <201292@live.nl>
2014-08-11 22:07:05 +0200

    worked on my game testing, a green block (player) and blue blocks as tile tests. next read a file and build from there.

--- pygame-plat-form-test/GameObjects.py
+++ pygame-plat-form-test/GameObjects.py
@@ -1,8 +1,24 @@
+import pygame
+from pygame.locals import *
+
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
+
+tile_height = 16
+tile_width = tile_height
+
+window_height = 480
+window_width = 640
+
+platform_base_height = 40
+
+leftwall  = 1   #(1,0,0,0)
+rightwall = 2   #(0,1,0,0)
+topwall   = 3   #(0,0,1,0)
+botwall   = 4   #(0,0,0,1)
 
 class GameObject(object):
 	def __init__(self, x, y):
@@ -19,21 +35,81 @@ class GameBlock(GameObject):
 		super(GameBlock, self).__init__(x, y)
 		self.width = width;
 		self.height = height;
-	def getRect():
+	def getRect(self):
 		return (self.x, self.y, self.width, self.height)
-	def setRect(x, y, width, height):
+	def setRect(self, x, y, width, height):
 		self.x = x
 		self.y = y
 		self.width = width
 		self.height = height
-	def draw(surface):
-		pygame.draw.rect(surface, green, self.getRect(), 0)
+	def handleCollisions(self, gameobjects):
+		pass
+	def update(self):
+		pass
+	def handleEvents(self, event):
+		pass
+	def draw(self, surface):
+		pygame.draw.rect(surface, blue, self.getRect(), 0)
 
 class GameTile(GameBlock):
 	def __init__(self, x, y, width, height, tile):
 		super(GameTile, self).__init__(x, y, width, height)
 		self.tile = tile
-	def getTile():
-		return self.tile
-	def setTile(tile):
+	def getTile(self):
+		return (self.tile)
+	def setTile(self, tile):
 		self.tile = tile
+
+class GameMob(GameBlock):
+	def __init__(self, x, y, width, height, spriteset):
+		super(GameMob, self).__init__(x, y, width, height)
+		self.spriteset = spriteset
+	def getTileset(self):
+		return (self.spriteset)
+	def setTileset(self, spriteset):
+		self.spriteset = spriteset
+
+class companionCube(GameMob):
+	def __init__(self, x, y, width, height, spriteset):
+		super(GameMob, self).__init__(x, y, width, height)
+		self.spriteset = spriteset
+		
+		self.moveLeft = False
+		self.moveRight = False
+		
+		self.jumpHeight = 0
+		self.jumping = False
+		
+		self.base = window_height - platform_base_height
+		self.gameobjects = None
+	def getTileset(self):
+		return (self.spriteset)
+	def setTileset(self, spriteset):
+		self.spriteset = spriteset
+	def handleEvents(self, event):
+		#handle all events moving in this case
+		if event.type == KEYDOWN:
+			if event.key == K_a:
+				self.moveLeft = True
+			elif event.key == K_d:
+				self.moveRight = True
+			if event.key == K_SPACE:
+				self.jumping = True
+			if event.key == K_p and self.gameobjects:
+				for tile in self.gameobjects:
+					print type(tile)
+		if event.type == KEYUP:
+			if event.key == K_a:
+				self.moveLeft = False
+			elif event.key == K_d:
+				self.moveRight = False
+	#a function that update this that move and sprites and stuff.
+	def update(self):
+		if self.moveRight:
+			self.setPos(self.getPos()[0]+1, self.getPos()[1]);
+		if self.moveLeft:
+			self.setPos(self.getPos()[0]-1, self.getPos()[1]);
+	def draw(self, surface):
+		pygame.draw.rect(surface, green, self.getRect(), 0)
+	def handleCollisions(self, gameobjects):
+		self.gameobjects = gameobjects
--- pygame-plat-form-test/main.py
+++ pygame-plat-form-test/main.py
@@ -2,21 +2,48 @@ import pygame, sys
 from pygame.locals import *
 from GameObjects import *
 
-block  = GameBlock(10,10,60,30);
-gameBlocks = []
-gameBlocks.append(block)
+mob  = companionCube(10,10,16,33, None);
+gameobjects = []
+gameobjects.append(mob)
+for i in range(0,window_width, 16):
+	tile = GameTile(i,window_height-tile_height, 14, tile_height, None)
+	gameobjects.append(tile)
 
 pygame.init()
 
 windowSurface = pygame.display.set_mode((640,480))
 pygame.display.set_caption("this is a game")
 
-def drawObjects():
-	for gameBlock in gameBlocks:
-		gameBlocks.draw(windowSurface);
+def drawObjects(surface):
+	for gameobject in gameobjects:
+		gameobject.draw(surface)
+
+def handleEvents(event):
+	for gameobject in gameobjects:
+		gameobject.handleEvents(event)
+
+def handleColitions():
+	for gameobject in gameobjects:
+		gameobject.handleCollisions(gameobjects)
+
+def updateObjects():
+	for gameobject in gameobjects:
+		gameobject.update()
 
 def main():
-	pass
+	running = True
+	while running:
+		windowSurface.fill(black)
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				running = False
+			else:
+				handleEvents(event)
+		handleColitions()
+		updateObjects()
+		drawObjects(windowSurface)
+		pygame.display.update()
+	pygame.quit()
 
 if __name__ == "__main__":
 	main()
