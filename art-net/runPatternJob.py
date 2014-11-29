18938799f69b74a2ca94b6645d215fbebca8dec9
Robert <201292@live.nl>
2014-11-29 21:02:54 +0100

    worked alot of things out made a readme ! do definitly read ! made lots readable. made runPatternJob.py catch ctrl-C to be sure things are closed if interupted.

--- art-net/runPatternJob.py
+++ art-net/runPatternJob.py
@@ -1,10 +1,10 @@
 import argparse, socket, time
-import imp
+import imp, signal, sys
 
 from artnet import buildPacket
 from convert import convertSnakeModes
 
-UDP_PORT = 6454
+UDP_PORT = 6453
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--delay", help="controlle flow speed.", metavar="<delay>", nargs="?", default=0.15, type=float)
@@ -23,6 +23,13 @@ TARGETS = config.TARGETS
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
 
+def signal_handler(signal, frame):
+	print "\nCaught Ctrl-C closing connections."
+	sock.close()
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 while(True):
 	for t in TARGETS:
 		pattern = TARGETS[t]
@@ -30,3 +37,4 @@ while(True):
 		data = convertSnakeModes(data)
 		sock.sendto(buildPacket(0, data), (t, UDP_PORT))
 	time.sleep(args.delay)
+sock.close()
